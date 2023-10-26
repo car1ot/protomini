567c8b59929ad976edd87a6594fbf0df519d4515
car1ot <car1ot635@gmail.com>
2023-10-26 16:11:27 +0300

    feat: add more types supported

--- src/index.test.ts
+++ src/index.test.ts
@@ -43,3 +43,51 @@ test("should encode and decode packets with boolean values", () => {
   expect(encoded).toBe('{"f":true}');
   expect(decoded).toEqual(originalPacket);
 });
+
+test("should encode and decode packets with string values", () => {
+  const p = new ProtoMini({ string: "s" });
+  const originalPacket = { string: "hello" };
+  const encoded = p.encodePacket(originalPacket);
+  const decoded = p.decodePacket(encoded);
+  expect(encoded).toBe('{"s":"hello"}');
+  expect(decoded).toEqual(originalPacket);
+});
+
+test("should encode and decode packets with number values", () => {
+  const p = new ProtoMini({ number: "n" });
+  const originalPacket = { number: 42 };
+  const encoded = p.encodePacket(originalPacket);
+  const decoded = p.decodePacket(encoded);
+  expect(encoded).toBe('{"n":42}');
+  expect(decoded).toEqual(originalPacket);
+});
+
+test("should encode and decode packets with BigInt values", () => {
+  const p = new ProtoMini({ bigInt: "b" });
+  const originalPacket = { bigInt: BigInt(9007199254740991) };
+  const encoded = p.encodePacket(originalPacket);
+  const decoded = p.decodePacket(encoded);
+  expect(encoded).toBe('{"b":"9007199254740991"}');
+  expect(decoded).toEqual({ bigInt: "9007199254740991" }); // BigInt is converted to string during encoding
+});
+
+test("should encode and decode packets with boolean values", () => {
+  const p = new ProtoMini({ boolean: "b" });
+  const originalPacket = { boolean: true };
+  const encoded = p.encodePacket(originalPacket);
+  const decoded = p.decodePacket(encoded);
+  expect(encoded).toBe('{"b":true}');
+  expect(decoded).toEqual(originalPacket);
+});
+
+test("should throw error for unsupported types", () => {
+  const p = new ProtoMini({});
+  // @ts-ignore
+  expect(() => p.encodePacket(() => {})).toThrow("Unsupported type: function");
+  // @ts-ignore
+  expect(() => p.encodePacket(Symbol())).toThrow("Unsupported type: symbol");
+  // @ts-ignore
+  expect(() => p.encodePacket(undefined)).toThrow(
+    "Unsupported type: undefined"
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,50 @@
-export class ProtoMini {
-  private keysMapping: { [key: string]: string };
+type KeyMapping = { [key: string]: string };
+type Primitive = string | number | BigInt | boolean;
+type Encodable = Primitive | Encodable[] | { [key: string]: Encodable };
 
-  constructor(keysMapping: { [key: string]: string }) {
+export class ProtoMini {
+  private keysMapping: KeyMapping;
+
+  constructor(keysMapping: KeyMapping) {
     this.keysMapping = keysMapping;
   }
 
-  public encodePacket(packet: any): string {
+  public encodePacket(packet: Encodable): string {
     const encodedPacket = this.replaceKeysAndValues(packet, true);
     return JSON.stringify(encodedPacket);
   }
 
-  public decodePacket(encodedPacket: string): any {
+  public decodePacket(encodedPacket: string): Encodable {
     const packet = JSON.parse(encodedPacket);
     return this.replaceKeysAndValues(packet, false);
   }
 
-  private replaceKeysAndValues(obj: any, encode: boolean): any {
+  private replaceKeysAndValues(obj: Encodable, encode: boolean): Encodable {
+    const type = typeof obj;
+
+    if (["function", "symbol", "undefined"].includes(type)) {
+      throw new Error(`Unsupported type: ${type}`);
+    }
+
     if (Array.isArray(obj)) {
       return obj.map((item) => this.replaceKeysAndValues(item, encode));
     }
 
-    if (typeof obj === "object") {
-      const newObj: { [key: string]: any } = {};
+    if (type === "object") {
+      const newObj: { [key: string]: Encodable } = {};
       for (const [key, value] of Object.entries(obj)) {
         const newKey = encode
           ? this.keysMapping[key] || key
           : Object.keys(this.keysMapping).find(
               (k) => this.keysMapping[k] === key
             ) || key;
-        newObj[newKey] = this.replaceKeysAndValues(value, encode);
+        newObj[newKey] = this.replaceKeysAndValues(value as Encodable, encode);
       }
       return newObj;
     }
 
-    if (typeof obj === "string") {
-      return encode
-        ? this.keysMapping[obj] || obj
-        : Object.keys(this.keysMapping).find(
-            (k) => this.keysMapping[k] === obj
-          ) || obj;
+    if (typeof obj === "bigint") {
+      return encode ? obj.toString() : BigInt(obj);
     }
 
     return obj;
